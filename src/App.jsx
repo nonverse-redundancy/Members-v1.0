d623c78adff3698f6e3e4da0ecd10c341428741f
Isuru Abhayaratne <isuru2003a@gmail.com>
2021-06-29 16:49:49 +1000

    Initialize App

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+// React
+import React, { useEffect, useState } from 'react';
+
+// Auth
+import auth from './api/auth/auth';
+function App() {
+  
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  useEffect(() => {
+
+  }, [])
+
+  return (
+    <div>
+      <h1>Testing</h1>
+    </div>
+  )
+}
+
+export default App
