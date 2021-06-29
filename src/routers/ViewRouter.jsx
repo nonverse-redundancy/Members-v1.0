3249fe94c0cf79e93dcd47b2f45acaaa95d164ff
Isuru Abhayaratne <isuru2003a@gmail.com>
2021-06-30 00:57:30 +1000

    Make ViewRouter

---
+++ src/routers/ViewRouter.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+
+import test from './test';
+
+export const ViewRouter = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+          <Route ></Route>
+      </Switch>
+    </BrowserRouter>
+  );
+};
