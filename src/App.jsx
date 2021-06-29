dfd983c5bbbffee246d59c83a747e40a9d62a45c
Isuru Abhayaratne <isuru2003a@gmail.com>
2021-06-29 17:22:34 +1000

    Test Auth

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,35 @@ import auth from './api/auth/auth';
 
 // Styles
 import './sass/app.scss'
+
+// Components 
+import Loader from './components/Loader'
+
 function App() {
 
+const [isAuthenticated, setIsAuthenticated] = useState(false);
 
+useEffect(async () => {
+  await auth.check()
+  if (auth.authenticated) {
+    setIsAuthenticated(true)
+  } else {
+    auth.login()
+  }
+})
 
-  return (
-    <div>
-      <h1>Dashboard</h1>
-    </div>
-  )
+  if (isAuthenticated) {
+    return (
+      <div className="app">
+        <h1>Dashboard</h1>
+        <button onClick={() => {auth.logout()}}>Logout</button>
+      </div>
+    )
+  } else {
+    return (
+      <Loader />
+    )
+  }
 }
 
 export default App
