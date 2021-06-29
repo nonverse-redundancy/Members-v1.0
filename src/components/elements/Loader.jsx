27b9df8c079e0bf7e92f233f6af312211e6bac99
Isuru Abhayaratne <isuru2003a@gmail.com>
2021-06-30 02:51:06 +1000

    Added a load message to be displayed after 5 seconds of loading

--- src/components/elements/Loader.jsx
+++ src/components/elements/Loader.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 
 import { ClipLoader } from 'react-spinners';
 
 function Loader() {
 
     const loading = true
+    const [delayed, setDelayed] = useState(false);
+
+    useEffect(() => {
+        setTimeout(() => {
+            setDelayed(true)
+        }, 5000);
+    })
 
     return (
-        <div className="loader">
-            <ClipLoader size={50} color={"#4995FD"} loading={loading}/>
+        <div className="loader-cont">
+            <div className="loader">
+                <ClipLoader size={50} color={"#4995FD"} loading={loading}/>
+            </div>
+            {delayed ? (
+                <span className="load-warn-svr text-primary">We seem to be having trouble connecting to our servers</span>
+            ) : ''}
         </div>
     )
 }
