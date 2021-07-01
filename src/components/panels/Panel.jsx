dc2fd525ff85dcba2be48eb5a929abe165301bad
Isuru Abhayaratne <isuru2003a@gmail.com>
2021-07-01 14:19:54 +1000

    Make panel component

---
+++ src/components/panels/Panel.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Panel = () => {
+
+    return (
+        <div className="panel-cont">
+            <div className="panel">
+
+            </div>
+        </div>
+    )
+}
+
+export default Panel
