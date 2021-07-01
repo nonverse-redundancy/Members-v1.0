e775a6bc12204ff766eb40507da72d66b1b6b8e8
Isuru Abhayaratne <isuru2003a@gmail.com>
2021-07-01 14:32:58 +1000

    Add temporary logout button

--- src/components/elements/Nav.jsx
+++ src/components/elements/Nav.jsx
@@ -1,6 +1,7 @@
 // React
 import React from "react";
 import { useHistory } from "react-router-dom";
+import auth from "../../api/auth/auth";
 
 import nvlogo from "../../assets/svg/nvlogo.svg";
 // Components
@@ -21,7 +22,9 @@ const SidePanel = () => {
           <i className="nav-link fas fa-envelope" id="messages"></i>
           <i className="nav-link fas fa-check" id="todo"></i>
         </div>
-        <i className="nav-link fas fa-cog" id="settings"></i>
+        <i className="nav-link fas fa-cog" id="settings" onClick={() => {
+          auth.logout() //NOTE This is a temporary logout button and should be removed
+        }}></i>
       </div>
     </div>
   );
