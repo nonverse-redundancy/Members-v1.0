7955f0dbd683693ef6e8944ab5e76622e39e4b26
Isuru Abhayaratne <isuru2003a@gmail.com>
2021-07-05 16:42:38 +1000

    Update Dashboard name with logged in user

--- src/components/dashboards/WelcomeContainer.jsx
+++ src/components/dashboards/WelcomeContainer.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import { useEffect, useState } from 'react';
+
+import user from '../../api/user/UserData';
+import auth from '../../api/auth/auth';
 
 const WelcomeContainer = () => {
 
     document.title = 'Nonverse | Home'
 
+    const [name, setName] = useState(false)
+
+    useEffect(async () => {
+        await user.get(auth.uuid)
+        setName(user.firstname)
+    })
+
     return (
         <div className="dashboard dash-welcome">
-            <h1>Welcome back {`Isuru`}</h1>
+            <h1>Welcome back {name}</h1>
         </div>
     )
 }
