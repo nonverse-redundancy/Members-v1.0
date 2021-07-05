b7cadb24814abd72bf0ede03ff68429207f3e181
Isuru Abhayaratne <isuru2003a@gmail.com>
2021-07-05 16:42:16 +1000

    Add Logic to get UserData from API

--- src/api/user/UserData.js
+++ src/api/user/UserData.js
@@ -1,41 +1,53 @@
 import axios from "axios";
 
 class user {
-    constructor() {
-        // API server location
-        this.apiurl = 'http://api.nonverse.test/';
+  constructor() {
+    // API server location
+    this.apiurl = "http://api.nonverse.test/";
 
-        // Default user variable
-        this.connected = false; // Was connection to API successful?
-        this.name = false; // Real (human) name of requested user
-        this.alias = false; // Username of requested user
-        this.email = false; // Email of requested user
-        this.details = false; // Additional details of requested user
+    // Default user variable
+    this.connected = false; // Was connection to API successful?
+    this.firstname = false; // Real (human) name of requested user
+    this.lastname = false; // Real (human) name of requested user
+    this.alias = false; // Username of requested user
+    this.email = false; // Email of requested user
+    this.details = false; // Additional details of requested user
 
+    // Config
+    axios.defaults.withCredentials = true;
+    this.csrf = `${this.apiurl}sanctum/csrf-cookie`; // Location of CSRF token
+  }
 
-        // Config
-        axios.defaults.withCredentials = true;
-    }
+  // Reset user variables
+  reset() {
+    this.firstname = false;
+    this.lastname = false;
+    this.alias = false;
+    this.email = false;
+    this.details = false;
+  }
 
-    // Reset user variables
-    reset() {
-        this.name = false;
-        this.alias = false;
-        this.email = false;
-        this.details = false;
-    }
-
-    // Get user data from API
-    async get(uuid) {
-        await axios
-        .get(`${this.apiurl}protected/user/${uuid}`)
-        .then((response) => {
-            //
-        })
-        .catch((e) => {
-            //
-        })
-    }
+  // Get user data from API
+  async get(uuid) {
+    await axios.get(this.csrf);
+    await axios
+      .get(`${this.apiurl}api/test/user/${uuid}`, {})
+      .then((response) => {
+        console.log(response);
+        this.firstname = response.data.name_first;
+        this.lastname = response.data.name_last;
+        this.alias = response.data.username;
+        this.email = response.data.email;
+        this.details = {
+          language: response.data.language,
+          created: 'Unavailable',
+          updates: 'Unavailable',
+        }
+      })
+      .catch((e) => {
+        //
+      });
+  }
 }
 
-export default new user
+export default new user();
