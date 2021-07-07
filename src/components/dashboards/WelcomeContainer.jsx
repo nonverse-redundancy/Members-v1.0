6adb0b0d93a7b08f019ea91cb0f80726826a7bf8
Isuru Abhayaratne <isuru2003a@gmail.com>
2021-07-08 01:59:24 +1000

    Show current date on welcome dashboard

--- src/components/dashboards/WelcomeContainer.jsx
+++ src/components/dashboards/WelcomeContainer.jsx
@@ -2,14 +2,19 @@ import React from 'react';
 import { useEffect, useState } from 'react';
 
 import user from '../../api/user/user';
+import calendar from '../../scripts/calendar';
 
 const WelcomeContainer = () => {
 
     document.title = 'Nonverse | Home'
 
+    // Calendar
+    var today = new Date();
+    
     return (
         <div className="dashboard dash-welcome">
             <h1>Welcome back {user.firstname}</h1>
+            <h2>Its {calendar.today('named-noyear')}</h2>
         </div>
     )
 }
