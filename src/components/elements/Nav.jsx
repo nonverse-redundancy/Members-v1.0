e2fb25ffeaaf86ab285fbebb80c942fe94c1a6c7
Isuru Abhayaratne <isuru2003a@gmail.com>
2021-07-07 18:24:35 +1000

    Set 'alt' prop for Nonverse Logo

--- src/components/elements/Nav.jsx
+++ src/components/elements/Nav.jsx
@@ -8,14 +8,13 @@ import nvlogo from "../../assets/svg/nvlogo.svg";
 // Components
 
 const SidePanel = () => {
-  //
 
   const history = useHistory();
 
   return (
     <div className="nav-cont">
       <div className="nav">
-        <img src={nvlogo} className="logo" />
+        <img src={nvlogo} alt="Nonverse-Logo" className="logo" />
         <div className="pg-link">
           <i className="nav-link fas fa-home" id="home"></i>
           <i className="nav-link fas fa-shield-alt" id="admin"></i>
