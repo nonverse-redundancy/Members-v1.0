0d0d4f7552ee9e807cf941a3047d0c624c9ce2b9
Isuru Abhayaratne <isuru2003a@gmail.com>
2021-07-08 14:03:05 +1000

    Pass in AccountButton as a child into TopNav

--- src/routers/DashboardRouter.jsx
+++ src/routers/DashboardRouter.jsx
@@ -8,6 +8,7 @@ import SecureAuth from "../components/widgets/SecureAuth";
 import Loader from "../components/elements/Loader";
 
 import WelcomeContainer from "../components/dashboards/WelcomeContainer";
+import AccountButton from "../components/buttons/AccountButton";
 
 export const DashboardRouter = () => {
   return (
@@ -15,7 +16,9 @@ export const DashboardRouter = () => {
       <div className="cont">
         <Nav />
         <div className="dashboard-cont">
-          <TopNav />
+          <TopNav>
+            <AccountButton/>
+          </TopNav>
           <Switch>
             <Route exact path="/" component={WelcomeContainer} />
             <Route path="*" component={Loader} />
