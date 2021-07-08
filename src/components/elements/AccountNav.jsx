7bce809c01b10e96bb4d2e3a9c5b60a17eddfca8
Isuru Abhayaratne <isuru2003a@gmail.com>
2021-07-08 15:23:06 +1000

    Make AccountNav

--- src/components/elements/AccountNav.jsx
+++ src/components/elements/AccountNav.jsx
@@ -1,18 +1,39 @@
 // React
-import React from 'react';
-import { useHistory } from 'react-router';
+import React from "react";
+import { useHistory } from "react-router";
+import { NavLink } from "react-router-dom";
 
-import nvlogo from "../../assets/svg/nvlogo.svg"
+import nvlogo from "../../assets/svg/nvlogo-dark.svg";
+import auth from "../../api/auth/auth";
 
 const AccountNav = () => {
+  const history = useHistory();
 
-    return (
-        <div className="nav-cont">
-            <div className="nav account-nav">
-                <img src={nvlogo} alt="Nonverse-Logo" className="logo"/>
-            </div>
+  return (
+    <div className="nav-cont">
+      <div className="nav account-nav">
+        <img src={nvlogo} alt="Nonverse-Logo" className="logo" />
+        <div className="pg-link">
+          <NavLink exact to="/account" activeClassName="active">
+            Account
+          </NavLink>
+          <NavLink to="/account/services" activeClassName="active">
+            Services
+          </NavLink>
+          <NavLink to="/account/subscriptions" activeClassName="active">
+            Subscriptions
+          </NavLink>
+          <NavLink to="/account/payments" activeClassName="active">
+            Payments
+          </NavLink>
+          <NavLink to="/support" activeClassName="active">
+            Support
+          </NavLink>
         </div>
-    )
-}
+        <a className="logout" onClick={() => {auth.logout()}}>Logout</a>
+      </div>
+    </div>
+  );
+};
 
-export default AccountNav
+export default AccountNav;
