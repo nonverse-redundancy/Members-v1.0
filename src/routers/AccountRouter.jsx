1951df9fc0c597c6da7b329a11449e9b2631be9a
Isuru Abhayaratne <isuru2003a@gmail.com>
2021-07-08 15:24:05 +1000

    Make base AccountRouter

--- src/routers/AccountRouter.jsx
+++ src/routers/AccountRouter.jsx
@@ -1,13 +1,27 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
+import WelcomeContainer from "../components/dashboards/WelcomeContainer";
 
 import AccountNav from "../components/elements/AccountNav";
+import Loader from "../components/elements/Loader";
+
+import TopNav from "../components/elements/TopNav";
+
+import HomeButton from "../components/buttons/HomeButton";
 
 export const AccountRouter = () => {
   return (
     <BrowserRouter>
       <div className="cont">
           <AccountNav />
+          <div className="account-cont">
+              <TopNav>
+                  <HomeButton />
+              </TopNav>
+              <Switch>
+                  <Route path="*" component={Loader} />
+              </Switch>
+          </div>
       </div>
     </BrowserRouter>
   );
