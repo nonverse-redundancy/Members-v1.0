e84f97aa3c6faa289b6f3d6d884e60531c53fd57
Isuru Abhayaratne <isuru2003a@gmail.com>
2021-07-09 23:37:24 +1000

    Add dynamic input validation to 'Name' and 'Email' fields

--- src/components/dashboards/account/PersonalSection.jsx
+++ src/components/dashboards/account/PersonalSection.jsx
@@ -1,89 +1,161 @@
-import React from 'react';
-import { useState } from 'react';
-import { useHistory } from 'react-router';
+import React, { useReducer } from "react";
+import { useState } from "react";
+import { useHistory } from "react-router";
 
 import auth from "../../../api/auth/auth";
 import user from "../../../api/user/user";
+import validate from "../../../api/user/validate";
 
 const PersonalSection = () => {
+  const history = useHistory();
 
-    const [isEdit, setIsEdit] = useState(false);
-    const [processing, setProcessing] = useState(false);
-    const [userData, setUserData] = useState({
-        name: user.fullname,
+  const [isEdit, setIsEdit] = useState(false);
+  const [processing, setProcessing] = useState(false);
+  const [userData, setUserData] = useState({});
+  const [error, setError] = useState(false);
+  const [done, setDone] = useState(false);
+  const [validator, setValidator] = useState(false);
+
+  var typeTimerName;
+  async function nameHandler(name) {
+    setProcessing(false);
+    setValidator({
+      ...validator,
+      name: false,
     });
-    const [error, setError] = useState(false);
-    const [done, setDone] = useState(false);
-
-    const history = useHistory()
-
-    async function editHander() {
-        if (isEdit) {
-            setProcessing(true)
-            
-            await user.update(auth.uuid, userData)
-            await user.get(auth.uuid)
-
-            setError(user.error)
-            if (user.status === 200) {
-                setDone(true)
-                setTimeout(() => {
-                    setDone(false);
-                }, 3000)
-            }
-            
-            setProcessing(false)
-            history.push('#')
-        } else {
-            setError(false);
-            setDone(false);
-            setUserData({
-                name: user.fullname,
+    if (name !== user.fullname) {
+        await validate.name(name, user.fullname);
+        if (validate.name.error) {
+            setProcessing(true);
+            setValidator({
+                ...validator,
+                name: validate.name.error,
             })
-
         }
-        setIsEdit(!isEdit);
+    } else {
+      setProcessing(false);
     }
-    
-    return (
-        <div className="a-sec sec-personal">
-            <h1>Personal Information</h1>
-            {error ? <span className="danger">{error}</span> : ''}
-            {done ? <span className="success">Account Updated</span> : ''}
-            <div className="info">
-                <div className="l">
-                    <h4 className="i">Name</h4>
-                    <h4 className="i">Username</h4>
-                    <h4 className="i">Email</h4>
-                    <h4 className="i">Birthday</h4>
-                    <h4 className="i">Phone</h4>
-                </div>
-                <div className="r">
-                    <form className={`out ${isEdit ? 'isedit' : ''}`} action="">
-                        <input type="text" className="i" value={isEdit ? userData.name : user.fullname} readOnly={!isEdit} onChange={(e) => {
+  }
+
+  var typeTimerEmail;
+  async function emailHandler(email) {
+    setProcessing(false);
+    setValidator({
+      ...validator,
+      email: false,
+    });
+    if (email !== user.email) {
+        await validate.email(email, user.email);
+        if (validate.email.error) {
+            setProcessing(true)
+            setValidator({
+                ...validator,
+                email: validate.email.error,
+            })
+        }
+    } else {
+        setProcessing(false);
+    }
+  }
+
+  async function editHander() {
+    if (isEdit) {
+      setProcessing(true);
+
+      await user.update(auth.uuid, userData);
+      await user.get(auth.uuid);
+
+      setError(user.update.error);
+      if (user.update.status === 200) {
+        setDone(true);
+        setTimeout(() => {
+          setDone(false);
+        }, 3000);
+      }
+      setTimeout(() => {
+        setProcessing(false);
+      }, 500)
+      history.push("#");
+    } else {
+      setError(false);
+      setDone(false);
+      setUserData({
+        name: user.fullname,
+        alias: user.alias,
+        email: user.email,
+      });
+    }
+    setIsEdit(!isEdit);
+  }
+  
+  return (
+    <div className="a-sec sec-personal">
+        <h1>Personal Information</h1>
+        {error ? <span className="danger">{error}</span> : ''}
+        {done ? <span className="success">Account Updated</span> : ''}
+        {validator.email ? <span className="danger">{validator.email}</span> : ''}
+        {validator.name ? <span className="danger">{validator.name}</span> : ''}
+        <div className="info">
+            <div className="l">
+                <h4 className="i">Name</h4>
+                <h4 className="i">Username</h4>
+                <h4 className="i">Email</h4>
+                <h4 className="i">Birthday</h4>
+                <h4 className="i">Phone</h4>
+            </div>
+            <div className="r">
+                <form className={`out ${isEdit ? 'isedit' : ''}`} action="">
+                    <input type="text" className="i" value={isEdit ? userData.name : user.fullname} readOnly={!isEdit}
+                    onChange={(e) => {
+                        setUserData({
+                            ...userData,
+                            name: e.target.value,
+                        })
+                    }}
+                    onKeyDown={() => {
+                        clearTimeout(typeTimerName);
+                    }}
+                    onKeyUp={(e) => {
+                        clearTimeout(typeTimerName);
+                        typeTimerName = setTimeout(() => {
+                            nameHandler(e.target.value);
+                        }, 500)
+                    }}/>
+                    <input type="text" className="i" name="username" value={user.alias} readOnly={!isEdit}/>
+                    <div className="verified">
+                        <input type="text" className="i" value={isEdit ? userData.email : user.email} readOnly={!isEdit}
+                        onChange={(e) => {
                             setUserData({
                                 ...userData,
-                                name: e.target.value,
+                                email: e.target.value,
                             })
-                        }}/>
-                        <input type="text" className="i" name="username" value={user.alias} readOnly={!isEdit}/>
-                        <div className="verified">
-                            <input type="text" className="i" value={user.email} readOnly={!isEdit}/>
-                            <i className="fas fa-check success"></i>
-                        </div>
-                        <input type="text" className="i" value="Unavailable" readOnly={!isEdit}/>
-                        <div className="verified">
-                            <input type="text" className="i i-short" value="Unavailable" readOnly={!isEdit}/>
-                            <i className="fas fa-times danger"></i>
-                        </div>
-                    </form>
-                </div>
+                        }}
+                        onKeyDown={() => {
+                            clearTimeout(typeTimerEmail);
+                        }}
+                        onKeyUp={(e) => {
+                            clearTimeout(typeTimerEmail);
+                            typeTimerEmail = setTimeout(() => {
+                                emailHandler(e.target.value);
+                            }, 500)
+                        }
+                        }
+                        />
+                        <i className="fas fa-check success"></i>
+                    </div>
+                    <input type="text" className="i" value="Unavailable" readOnly={!isEdit}/>
+                    <div className="verified">
+                        <input type="text" className="i i-short" value="Unavailable" readOnly={!isEdit}/>
+                        <i className="fas fa-times danger"></i>
+                    </div>
+                </form>
             </div>
-            <a className={`edit ${processing ? 'isload' : ''}`} 
-               onClick={processing ? console.log('Processing') : () => {editHander()}}>{isEdit ? 'Save' : 'Edit'}
-            </a>
         </div>
-        )
-}
+        <a className={`edit ${processing ? 'isload' : ''}`} 
+           onClick={processing ? console.log('Processing') : () => {editHander()}}>{isEdit ? 'Save' : 'Edit'}
+        </a>
+    </div>
+    )
+};
 
-export default PersonalSection
+export default PersonalSection;
