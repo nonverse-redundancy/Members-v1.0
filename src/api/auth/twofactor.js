49eb18cd7137fc3a94a18cba051f08a5b0e37797
Isuru Abhayaratne <isuru2003a@gmail.com>
2021-07-17 13:58:44 +1000

    Make 2FA enable function

--- src/api/auth/twofactor.js
+++ src/api/auth/twofactor.js
@@ -4,14 +4,18 @@ import auth from "./auth";
 // Handle Google 2FA Via SecureAuth Servers
 class twofactor {
   constructor() {
+
+    // Default 2FA values
+    this.enable.success = false;
+
     // Config
     axios.defaults.withCredentials = true;
   }
 
   async setup() {
-    await auth._csrf;
+    await auth._csrf();
     await axios
-      .get(`${auth.authurl}user/${auth.uuid}/two-factor-url`, {})
+      .get(`${auth.authurl}user/${auth.uuid}/two-factor-setup`, {})
       .then((response) => {
         this.setup.url = response.data.url;
         this.setup.code = response.data.code;
@@ -19,6 +23,22 @@ class twofactor {
       .catch((e) => {
         //
       });
+  }
+
+  async enable(code) {
+    await auth._csrf();
+    await axios
+    .post(`${auth.authurl}user/${auth.uuid}/two-factor-setup`, {
+      uuid: auth.uuid,
+      one_time_password: code,
+    })
+    .then((response) => {
+      this.enable.success = true;
+    })
+    .catch((e) => {
+      console.log(e)
+    })
+
   }
 }
 
