b21e5143a7cf09283d6e2b37234b9d33f4715af6
Isuru Abhayaratne <isuru2003a@gmail.com>
2021-07-19 01:32:20 +1000

    Make 2FA disable function

--- src/api/auth/twofactor.js
+++ src/api/auth/twofactor.js
@@ -4,9 +4,9 @@ import auth from "./auth";
 // Handle Google 2FA Via SecureAuth Servers
 class twofactor {
   constructor() {
-
     // Default 2FA values
     this.enable.success = false;
+    this.disable.success = false;
 
     // Config
     axios.defaults.withCredentials = true;
@@ -28,17 +28,31 @@ class twofactor {
   async enable(code) {
     await auth._csrf();
     await axios
-    .post(`${auth.authurl}session/user/two-factor-setup`, {
-      uuid: auth.uuid,
-      one_time_password: code,
-    })
-    .then((response) => {
-      this.enable.success = true;
-    })
-    .catch((e) => {
-      console.log(e)
-    })
+      .post(`${auth.authurl}session/user/two-factor-setup`, {
+        uuid: auth.uuid,
+        one_time_password: code,
+      })
+      .then((response) => {
+        this.enable.success = true;
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }
 
+  async disable(password) {
+    await auth._csrf();
+    await axios
+      .post(`${auth.authurl}session/user/two-factor-disable`, {
+        password: password,
+      })
+      .then((response) => {
+        console.log(response.data);
+        this.disable.success = true;
+      })
+      .catch((e) => {
+        //
+      });
   }
 }
 
