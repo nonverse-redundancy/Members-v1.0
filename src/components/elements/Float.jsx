cdf6f2dc4adc628f44166588f95b583f0e6d5f44
Isuru Abhayaratne <isuru2003a@gmail.com>
2021-07-18 23:39:59 +1000

    Create float component

---
+++ src/components/elements/Float.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Float = ( { children } ) => {
+
+    return (
+        <div className="float warning-float">
+            {children}
+        </div>
+    )
+}
+
+export default Float
