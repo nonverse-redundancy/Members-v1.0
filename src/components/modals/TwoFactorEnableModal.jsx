49153240426fa97f637595d617e70b811b2f0849
Isuru Abhayaratne <isuru2003a@gmail.com>
2021-07-19 13:54:18 +1000

    Cleanup

--- src/components/modals/TwoFactorEnableModal.jsx
+++ src/components/modals/TwoFactorEnableModal.jsx
@@ -8,8 +8,7 @@ import twofactor from "../../api/auth/twofactor";
 import QRCode from "qrcode.react";
 import auth from "../../api/auth/auth";
 
-const TwoFactorEnableModal = ( { setTFA, close } ) => {
-
+const TwoFactorEnableModal = ({ setTFA, close }) => {
   const [loading, setLoading] = useState(true);
   const [twoFactorURL, setTwoFactorURL] = useState(false);
   const [showCode, setShowCode] = useState(false);
@@ -18,7 +17,7 @@ const TwoFactorEnableModal = ( { setTFA, close } ) => {
 
   async function enableTwoFactor() {
     setLoading(true);
-    await twofactor.enable(oneTimePassword)
+    await twofactor.enable(oneTimePassword);
     if (twofactor.enable.success) {
       setError(false);
       await auth.check();
@@ -32,11 +31,9 @@ const TwoFactorEnableModal = ( { setTFA, close } ) => {
   }
 
   useEffect(() => {
-
     let mounted = true;
     async function generateCode() {
-      await twofactor.setup()
-      .then(() => {
+      await twofactor.setup().then(() => {
         if (mounted) {
           setTwoFactorURL(twofactor.setup.url);
         }
@@ -50,66 +47,146 @@ const TwoFactorEnableModal = ( { setTFA, close } ) => {
 
     return () => {
       mounted = false;
-    }
-  }, [twoFactorURL])
+    };
+  }, [twoFactorURL]);
 
   return (
     <Modal close={close}>
       {loading ? (
-          <div className="await">
-              <div className="loader">
-                 <ClipLoader size={50} color={"#4995FD"} />
-              </div>
+        <div className="await">
+          <div className="loader">
+            <ClipLoader size={50} color={"#4995FD"} />
           </div>
+        </div>
       ) : (
         <div>
           <div className="content two-factor">
             <h1 className="head">Enable 2 Factor Authentication</h1>
             {showCode ? (
               <div className="layout layout-code">
-              <input type="text" placeholder="Code" 
-              onChange={(e) => {
-                setOneTimePassword(e.target.value)
-                setError(false);
-              }} />
-              <span className="danger"><br />{error ? 'Invalid Code' : ''}</span>
+                <input
+                  type="text"
+                  placeholder="Code"
+                  onChange={(e) => {
+                    setOneTimePassword(e.target.value);
+                    setError(false);
+                  }}
+                />
+                <span className="danger">
+                  <br />
+                  {error ? "Invalid Code" : ""}
+                </span>
                 <h1 className="code">{twofactor.setup.code}</h1>
                 <div className="out">
-                  <span className="splash">1. <span className="default">Enter above code into authenticator app of choice</span><br/></span>
-                      <div className="suggested">
-                        <span className="splash">Suggested by Nonverse | <a href="https://steptwo.app/" target="_blank" rel="noreferrer">Step Two</a></span>
-                      </div>
-                  <span className="splash">2. <span className="default">Enter 6 digit code provided by app</span><br/></span>
-                  <span className="splash">3. <span className="default">Click Verify</span></span>
+                  <span className="splash">
+                    1.{" "}
+                    <span className="default">
+                      Enter above code into authenticator app of choice
+                    </span>
+                    <br />
+                  </span>
+                  <div className="suggested">
+                    <span className="splash">
+                      Suggested by Nonverse |{" "}
+                      <a
+                        href="https://steptwo.app/"
+                        target="_blank"
+                        rel="noreferrer"
+                      >
+                        Step Two
+                      </a>
+                    </span>
+                  </div>
+                  <span className="splash">
+                    2.{" "}
+                    <span className="default">
+                      Enter 6 digit code provided by app
+                    </span>
+                    <br />
+                  </span>
+                  <span className="splash">
+                    3. <span className="default">Click Verify</span>
+                  </span>
                 </div>
-                <button className="toggle-code" onClick={() => {setShowCode(!showCode)}}>Scan QR Code</button>
+                <button
+                  className="toggle-code"
+                  onClick={() => {
+                    setShowCode(!showCode);
+                  }}
+                >
+                  Scan QR Code
+                </button>
               </div>
             ) : (
               <div className="layout">
                 <div className="qr">
-                  <QRCode renderAs={'svg'} bgColor={'#ECF0F3'} fgColor={"#333344"} value={twoFactorURL} />
-                  <button className="toggle-code" onClick={() => {setShowCode(!showCode)}}>Can't Scan?</button>
+                  <QRCode
+                    renderAs={"svg"}
+                    bgColor={"#ECF0F3"}
+                    fgColor={"#333344"}
+                    value={twoFactorURL}
+                  />
+                  <button
+                    className="toggle-code"
+                    onClick={() => {
+                      setShowCode(!showCode);
+                    }}
+                  >
+                    Can't Scan?
+                  </button>
                 </div>
                 <div className="out">
-                  <input type="text" placeholder="Code" 
-                  onChange={(e) => {
-                    setOneTimePassword(e.target.value)
-                    setError(false);
-                  }} />
-                  <span className="danger"><br />{error ? 'Invalid Code' : ''}</span>
-                    <ul>
-                      <li><span className="default">Scan QR code using authenticator app of choice</span></li>
-                      <div className="suggested">
-                        <span className="splash">Suggested by Nonverse | <a href="https://steptwo.app/" target="_blank" rel="noreferrer">Step Two</a></span>
-                      </div>
-                      <li><span className="default">Enter 6 digit code provided by app</span></li>
-                      <li><span className="default">Click Verify</span></li>
-                    </ul>
+                  <input
+                    type="text"
+                    placeholder="Code"
+                    onChange={(e) => {
+                      setOneTimePassword(e.target.value);
+                      setError(false);
+                    }}
+                  />
+                  <span className="danger">
+                    <br />
+                    {error ? "Invalid Code" : ""}
+                  </span>
+                  <ul>
+                    <li>
+                      <span className="default">
+                        Scan QR code using authenticator app of choice
+                      </span>
+                    </li>
+                    <div className="suggested">
+                      <span className="splash">
+                        Suggested by Nonverse |{" "}
+                        <a
+                          href="https://steptwo.app/"
+                          target="_blank"
+                          rel="noreferrer"
+                        >
+                          Step Two
+                        </a>
+                      </span>
+                    </div>
+                    <li>
+                      <span className="default">
+                        Enter 6 digit code provided by app
+                      </span>
+                    </li>
+                    <li>
+                      <span className="default">Click Verify</span>
+                    </li>
+                  </ul>
                 </div>
               </div>
             )}
           </div>
-          <button className="verify" onClick={() => {enableTwoFactor()}}>Verify</button>
+          <button
+            className="verify"
+            onClick={() => {
+              enableTwoFactor();
+            }}
+          >
+            Verify
+          </button>
         </div>
       )}
     </Modal>
