b52a7170e42ab6ebc1b8f554d430bdceaa30caa5
Isuru Abhayaratne <isuru2003a@gmail.com>
2021-07-20 00:55:09 +1000

    Make 'home' button on Nav go to home

--- src/components/elements/Nav.jsx
+++ src/components/elements/Nav.jsx
@@ -1,5 +1,6 @@
 // React
 import React from "react";
+import { useHistory } from "react-router-dom";
 
 import nvlogo from "../../assets/svg/nvlogo.svg";
 
@@ -7,12 +8,14 @@ import nvlogo from "../../assets/svg/nvlogo.svg";
 
 const SidePanel = () => {
 
+  const history = useHistory();
+
   return (
     <div className="nav-cont">
       <div className="nav dash-nav">
         <img src={nvlogo} alt="Nonverse-Logo" className="logo" />
         <div className="pg-link">
-          <i className="nav-link fas fa-home" id="home"></i>
+          <i className="nav-link fas fa-home" id="home" onClick={() => history.push("/")}></i>
           <i className="nav-link fas fa-shield-alt" id="admin"></i>
           <i className="nav-link fas fa-envelope" id="messages"></i>
           <i className="nav-link fas fa-check" id="todo"></i>
