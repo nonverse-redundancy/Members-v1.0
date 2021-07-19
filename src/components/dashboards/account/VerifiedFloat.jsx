5e68ce3fbc347eb4dd73523ab5ec3d0555d27e73
Isuru Abhayaratne <isuru2003a@gmail.com>
2021-07-19 17:03:01 +1000

    Check if user is verified

--- src/components/dashboards/account/VerifiedFloat.jsx
+++ src/components/dashboards/account/VerifiedFloat.jsx
@@ -1,9 +1,23 @@
+import axios from "axios";
 import React from "react";
 
 import auth from "../../../api/auth/auth";
 import Float from "../../elements/Float";
 
 const VerifiedFloat = () => {
+
+  async function sendVerification() {
+    await auth._csrf()
+    await axios
+    .post(`${auth.authurl}email/send-verification`)
+    .then((response) => {
+      console.log(response)
+    })
+    .catch((e) => {
+      console.log(e)
+    })
+  }
+
   return (
     <Float>
       { auth.verified ? (
@@ -12,7 +26,7 @@ const VerifiedFloat = () => {
         <span className="danger">
           Your email address is not verified
           <span className="default"> |</span>
-          <button className="link">resend email</button>
+          <button className="link" onClick={() => {sendVerification()}}>resend email</button>
         </span>
         )}
     </Float>
