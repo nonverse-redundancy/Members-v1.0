e186095b6abd91de88b727ceeb08f191e523a311
Isuru Abhayaratne <isuru2003a@gmail.com>
2021-07-21 02:00:15 +1000

    Show loaderbar on component load

--- src/components/dashboards/account/AccountContainer.jsx
+++ src/components/dashboards/account/AccountContainer.jsx
@@ -1,6 +1,9 @@
 import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 
 import user from "../../../api/user/user";
+import LoaderBar from "../../elements/LoaderBar";
 
 import PersonalSection from "./PersonalSection";
 import RecoverySection from "./RecoverySection";
@@ -8,17 +11,31 @@ import SecuritySection from "./SecuritySection";
 import VerifiedFloat from "./VerifiedFloat";
 
 const AccountContainer = () => {
+
+  const [load, setLoad] = useState(0)
+  const [complete, setComplete] = useState(false)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoad(100);
+      setTimeout(() => {
+        setComplete(true)
+      }, 1000)
+    }, 1)
+  })
+
   return (
     <div className="dashboard dash-account">
+    <LoaderBar load={load} complete={complete}/>
       <div className="head">
         <h1>Your Account</h1>
         <VerifiedFloat />
       </div>
       <h2>{user.fullname}</h2>
       <div className="account">
-        <PersonalSection />
+        <PersonalSection setLoad={setLoad} setComplete={setComplete}/>
         <SecuritySection />
-        <RecoverySection />
+        <RecoverySection setLoad={setLoad} setComplete={setComplete} />
       </div>
     </div>
   );
