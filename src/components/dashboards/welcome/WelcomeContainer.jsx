c27bd806cfaad768616cb2540395b27451e3ee93
Isuru Abhayaratne <isuru2003a@gmail.com>
2021-08-04 21:03:05 +1000

    Add "Loader" on render

--- src/components/dashboards/welcome/WelcomeContainer.jsx
+++ src/components/dashboards/welcome/WelcomeContainer.jsx
@@ -1,7 +1,10 @@
 import React from "react";
+import { useEffect } from "react";
+import { useState } from "react";
 
 import user from "../../../api/user/user";
 import calendar from "../../../scripts/calendar";
+import LoaderBar from "../../elements/LoaderBar";
 import VerifiedFloat from "../account/VerifiedFloat";
 import AccountSecuredTile from "./AccountSecuredTile";
 import NoServiceTile from "./NoServiceTile";
@@ -9,8 +12,21 @@ import NoServiceTile from "./NoServiceTile";
 const WelcomeContainer = () => {
   document.title = "Nonverse | Home";
 
+  const [load, setLoad] = useState(Math.floor(Math.random() * 100 + 5));
+  const [complete, setComplete] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setLoad(100);
+      setTimeout(() => {
+        setComplete(true);
+      }, 1000);
+    }, 1);
+  });
+
   return (
     <div className="dashboard dash-welcome">
+      <LoaderBar load={load} complete={complete}/>
       <div className="head">
         <h1>Welcome back {user.firstname}</h1>
         <VerifiedFloat />
@@ -19,6 +35,9 @@ const WelcomeContainer = () => {
       <div className="summary">
         <NoServiceTile />
         <AccountSecuredTile />
+      </div>
+      <div className="whatsnew">
+        {/* TODO Make "Whats New" section */}
       </div>
     </div>
   );
