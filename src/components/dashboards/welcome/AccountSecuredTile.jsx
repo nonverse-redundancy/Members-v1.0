9b65809a89453b621129cdb64f280f07501ff223
Isuru Abhayaratne <isuru2003a@gmail.com>
2021-08-04 20:36:05 +1000

    Add date placeholder

--- src/components/dashboards/welcome/AccountSecuredTile.jsx
+++ src/components/dashboards/welcome/AccountSecuredTile.jsx
@@ -1,11 +1,12 @@
 import React from "react";
 import DashboardTileSplash from "../../elements/DashboardTileSplash";
+import calendar from "../../../scripts/calendar";
 
 const AccountSecuredTile = () => {
 
     return (
         <DashboardTileSplash title="You account is secured" width="25" bg="linear-gradient(135deg, #EF5DA8 0%, #7879F1 100%)">
-            <p className="tile-info-sub">Last login: {}</p>
+            <p className="tile-info-sub">Last login: {calendar.today()}</p>
         </DashboardTileSplash>
     )
 };
