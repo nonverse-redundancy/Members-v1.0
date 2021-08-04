816587c58f213dbc6744696081f359601062c55b
Isuru Abhayaratne <isuru2003a@gmail.com>
2021-08-04 20:35:48 +1000

    Add CTA

--- src/components/dashboards/welcome/NoServiceTile.jsx
+++ src/components/dashboards/welcome/NoServiceTile.jsx
@@ -7,6 +7,9 @@ const NoServiceTile = () => {
   return (
     <DashboardTileSplash title="You're not connected to any of our services" width="30" bg="linear-gradient(135deg, #2FEBE0 0%, #9685FF 100%)">
         <p className="tile-info-sub">Your account isn't linked with any Nonverse services so we can't show you much</p>
+        <div className="cta-explore">
+          <span>Explore The Network</span>
+        </div>
     </DashboardTileSplash>
   );
 };
